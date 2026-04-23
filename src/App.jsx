d646ea022ba446186c9e7b12abe69dfc9663a98b
doraaaO <qzsb6dqswx@privaterelay.appleid.com>
2026-04-23 14:45:53 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom'
 
-// 匯入各分頁元件 (需自行建立對應的 jsx 檔案並貼入行程內容)
+// 匯入各分頁元件
 import Day1 from './pages/Day1'
 import Day2 from './pages/Day2'
 import Day3 from './pages/Day3'
@@ -9,30 +9,33 @@ import Day5 from './pages/Day5'
 import Info from './pages/Info'
 import Budget from './pages/Budget'
 
-// ...前方的 import 省略
-
 function App() {
   return (
-    {/* 1. 最外層必須是 app-container */ }
-    < div className = "app-container" >
-
-      {/* 2. 導航列必須在裡面，且 className 是 bottom-nav */ }
-      < nav className = "bottom-nav" >
+    <div className="app-container">
+      {/* 導航列必須在裡面，且 className 是 bottom-nav */}
+      <nav className="bottom-nav">
         <Link to="/">Day 1</Link>
+        <Link to="/day2">Day 2</Link>
         <Link to="/day3">Day 3</Link>
-  {/* ... 其他連結 */ }
-      </nav >
+        <Link to="/day4">Day 4</Link>
+        <Link to="/day5">Day 5</Link>
+        <Link to="/info">資訊</Link>
+        <Link to="/budget">預算</Link>
+      </nav>
 
-    {/* 3. 內容區塊 */ }
-    < main className = "content" >
-      <Routes>
-        <Route path="/" element={<Day1 />} />
-        <Route path="/day3" element={<Day3 />} />
-        {/* ... 其他路由 */}
-      </Routes>
-      </main >
-      
-    </div >
+      {/* 內容區塊 */}
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Day1 />} />
+          <Route path="/day2" element={<Day2 />} />
+          <Route path="/day3" element={<Day3 />} />
+          <Route path="/day4" element={<Day4 />} />
+          <Route path="/day5" element={<Day5 />} />
+          <Route path="/info" element={<Info />} />
+          <Route path="/budget" element={<Budget />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
