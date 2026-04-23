1a057a2b3d0bc1a74dee63a6a08fe0be1d6fff9e
doraaaO <qzsb6dqswx@privaterelay.appleid.com>
2026-04-23 11:57:36 +0800

    update

--- src/pages/Day1.jsx
+++ src/pages/Day1.jsx
@@ -1,7 +1,3 @@
-export default function Day1() {
-  return <div>Day 1 行程</div>;
-}
-
 import React from 'react';
 
 export default function Day1() {
--- src/pages/Day3.jsx
+++ src/pages/Day3.jsx
@@ -1,7 +1,3 @@
-export default function Day3() {
-  return <div>Day 3 行程</div>;
-}
-
 import React from 'react';
 
 export default function Day3() {
